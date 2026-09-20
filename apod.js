2b0eac0191f77a4fd51fa8ecc104c41b1403d671
Shaurya <shauryaofficial100@gmail.com>
2026-09-21 00:20:53 +0530

    feat: implement NASA APOD fetcher and display logic
    - Added BASEURL and APIKEY constants for NASA API access
    - Implemented showAPOD function to update DOM elements
    - Created loadToday and loadrandom functions for fetching image data
    - Handled conditional display logic for image and video media types

---
+++ apod.js
@@ -0,0 +1,42 @@
+const APIKEY ="Demo_Key";
+const BASEURL ="https://api.nasa.gov/planetry/apod";
+
+function showAPOD(data) {
+    document.getElementById('apod-title').textContent = data.title;
+    document.getElementById('apod-date').textContent = data.date;
+    document.getElementById('apod-desc').textContent =data.explanation;
+
+    const img = document.getElementById('apod-img');
+    const video = document.getElementById('apod-video');
+
+    if (data.media_type ==='video') {
+        img.hidden = true;
+        video.hidden = false;
+        video.src = data.url;
+
+    } else {
+        video.hidden = true;
+        img.hidden = false;
+        img.src= data.url;
+
+    }
+}
+
+function loadToday() {
+    fetch('${BASEURL}?apikey=${APIKEY}')
+    .then(res => res.json())
+    .then(data => showAPOD(data));
+}
+
+function loadrandom () {
+    const start= new date(1995, 5, 16);
+    const end = new Date();
+    const random = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()) );
+    const date = randomISOString().split('T')[0];
+
+    fetch('${BASEURL}?apikey=${APIKEY}&date=${date}')
+    .then(res => res.json())
+    .then(data => showAPOD(data));
+
+
+}
