e2319318f2ee7cec5253dad7155fdb8707be5c7e
Shaurya <shauryaofficial100@gmail.com>
2026-09-26 00:38:28 +0530

    chore: hardcode NASA API key in apod function

--- netlify/functions/apod.js
+++ netlify/functions/apod.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const date = event.queryStringParameters?.date || '';
     const dateParam = date ? `&date=${date}` : '';
-    const url = `https://api.nasa.gov/planetary/apod?apikey=${process.env.NASA_KEY}${dateParam}`;
+    const url = `https://api.nasa.gov/planetary/apod?apikey=zu01UBg5J0K5vgujsNjRWU05ws6v3xJBf82t7e5I${dateParam}`;
     const res = await fetch(url);
     const data = await res.json();
     return {
