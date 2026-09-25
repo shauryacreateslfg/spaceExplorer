6d3aec0c66e39ef78b7e5663f813ac1c05a22f95
Shaurya <shauryaofficial100@gmail.com>
2026-09-26 00:58:23 +0530

    chore: include debug_url in apod function response

--- netlify/functions/apod.js
+++ netlify/functions/apod.js
@@ -7,6 +7,6 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         headers: { 'Access-Control-Allow-Origin': '*' },
-        body: JSON.stringify(data)
+        body: JSON.stringify({ debug_url: url, response: data })
     };
 };
