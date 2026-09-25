ddbcd736e99e1c3f8d2bc8673b7f9b5223375138
Shaurya <shauryaofficial100@gmail.com>
2026-09-26 00:24:15 +0530

    chore: remove redundant node-fetch import and add optional chaining
    - Remove unused node-fetch import
    - Use optional chaining for queryStringParameters access

--- netlify/functions/apod.js
+++ netlify/functions/apod.js
@@ -1,7 +1,5 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
-    const date = event.queryStringParameters.date || '';
+    const date = event.queryStringParameters?.date || '';
     const dateParam = date ? `&date=${date}` : '';
     const url = `https://api.nasa.gov/planetary/apod?apikey=${process.env.NASA_KEY}${dateParam}`;
     const res = await fetch(url);
